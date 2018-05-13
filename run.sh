239b782a92c7d2a1d46b06a5c01011b530f54292
wildProgrammer <ianknote@gmail.com>
2018-05-13 14:30:15 +0300

    adding gateway and bike microservice

---
+++ run.sh
@@ -0,0 +1,3 @@
+./jhipster-registry-3.2.4.war --security.user.password=admin --jhipster.security.authentication.jwt.secret=secret-key 
+--spring.cloud.config.server.native.search-locations=file:./central-config
+
