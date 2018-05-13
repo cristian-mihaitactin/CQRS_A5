f33ef0d4e9e9cfef6b24ec6c5b668781df5aa79f
wildProgrammer <ianknote@gmail.com>
2018-05-14 00:14:17 +0300

    a couple of useless files deleted

--- run.sh
+++
@@ -1,3 +0,0 @@
-./jhipster-registry-3.2.4.war --security.user.password=admin --jhipster.security.authentication.jwt.secret=secret-key 
---spring.cloud.config.server.native.search-locations=file:./central-config
-
